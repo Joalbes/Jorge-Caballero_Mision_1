fbfb0b88dac69e66a089c2260d37e30827d32542
Jorge A Caballero <joalbes@gmail.com>
2023-03-22 18:56:20 -0500

    Entrega mision 3

--- Mision_3/main.js
+++ Mision_3/main.js
@@ -2,8 +2,23 @@
 
 //variables
 const crearUsuario = document.querySelector("#crearusuario");
+const contenidoTabla = document.querySelector("#contenidotabla");
 
 
 
 //eventos
 crearUsuario.addEventListener('click',()=>{location.href = 'index2.html'})
+
+
+let objListaPersonas = JSON.parse(localStorage.getItem("listaPersonas"));
+objListaPersonas.forEach(element => {
+    contenidoTabla.innerHTML = `
+    <tr>
+    <td class="px-[2rem]">${element.area}</td>
+    <td class="px-[2rem]">${element.nombre} ${element.apellidos}</td>
+    <td class="px-[2rem]">${element.usuario}</td>
+    <td class="px-[2rem]">${element.email}</td>
+    <td class="px-[2rem]">${element.edad}</td>
+  </tr>`
+    
+});
--- Mision_3/main2.js
+++ Mision_3/main2.js
@@ -35,21 +35,30 @@ const email  = document.querySelector("#email");
 const btnEnviar = document.querySelector("#btnenviar");
 const formulario = document.querySelector("#formulario");
 
-//Listas
-listaPersonas = [];
+//Listaslet listaPersonas = []
 
 
-//eventos
+//************************************eventos************************************//
 //bloquear formulario
 formulario.addEventListener('submit',(e)=>{
     e.preventDefault();
 })
-
+//guardar datos de 
 btnEnviar.addEventListener('click',()=>{
-    const persona = new Persona(nombres.value, apellidos.value, area.value, usuario.value, edad.value, direccion.value, email.value);
-    listaPersonas.push(persona);   
-    console.log(`Nombre: ${nombres.value}`);
-    //console.log(`Lista Personas: ${listaPersonas}`);
-    //localStorage.setItem('listaPersonas', listaPersonas);
+    //traer el obj JSOn que esta en el Local Storage y covertitir el objeto a lista y asignarlo a la lista de personas
+   let listaPersonas = JSON.parse(localStorage.getItem('listaPersonas')) || [];   
+
+    //creo objeto personas
+    let persona = new Persona(nombres.value, apellidos.value, area.value, usuario.value, edad.value, direccion.value, email.value);
+    //pusheo elobj persona a la lista de personas
+    listaPersonas.push(persona);
+    //transformo en JSON la lista de personas
+    listaPersonas = JSON.stringify(listaPersonas);
+    //guardo en el loca storage el obj JSON de la lista de personas
+    localStorage.setItem("listaPersonas", listaPersonas);
+    //volver a la pag principal.
+    location.href = "index.html"; 
+    
 })
+
 
