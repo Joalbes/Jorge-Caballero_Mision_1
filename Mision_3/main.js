9cbf0572e59d9bcfcc40c3fcbbcb35bedc87b8cf
Jorge Caballero <joalbes@gmail.com>
2023-03-23 13:56:14 -0500

    Deuda tecnica mision 3

--- Mision_3/main.js
+++ Mision_3/main.js
@@ -3,22 +3,70 @@
 //variables
 const crearUsuario = document.querySelector("#crearusuario");
 const contenidoTabla = document.querySelector("#contenidotabla");
+const buscarPorNombre = document.querySelector("#buscarPorNombre");
+
+let contadorCoincidencias = 0;
+let posiciones = [];
 
 
 
 //eventos
-crearUsuario.addEventListener('click',()=>{location.href = 'index2.html'})
+crearUsuario.addEventListener('click', () => { location.href = 'index2.html' })
+buscarPorNombre.addEventListener('keyup', filtrar)
+
+//Logica
+crearTabla();
 
 
-let objListaPersonas = JSON.parse(localStorage.getItem("listaPersonas"));
-objListaPersonas.forEach(element => {
-    contenidoTabla.innerHTML = `
-    <tr>
-    <td class="px-[2rem]">${element.area}</td>
-    <td class="px-[2rem]">${element.nombre} ${element.apellidos}</td>
-    <td class="px-[2rem]">${element.usuario}</td>
-    <td class="px-[2rem]">${element.email}</td>
-    <td class="px-[2rem]">${element.edad}</td>
-  </tr>`
-    
-});
+//funciones
+
+function limpiarTabla() {
+  contenidoTabla.innerHTML = ''
+}
+
+function filtrar(e) {
+  if (e.keyCode === 'Enter') {
+    //Se obtiene la lista de personas
+    let nombreABuscar = buscarPorNombre.value;
+    let objListaPersonas = JSON.parse(localStorage.getItem("listaPersonas"));    
+    //se crea lista que contiene los indices de coincidencias
+    objListaPersonas.forEach(element => {
+    contadorCoincidencias++;
+    if (element.nombre = nombreABuscar) {
+      posiciones.push(contadorCoincidencias);
+       }
+    }); 
+    //Imprimo en la row de la table el elemento coincidente
+    if (posiciones.length > 0) {
+      posiciones.forEach(element => {
+      contenidoTabla.innerHTML +=`
+                                  <tr>
+                                    <td class="px-[3rem]">${objListaPersonas[element].area}</td>
+                                    <td class="px-[3rem]">${objListaPersonas[element].nombre} ${objListaPersonas[element].apellidos}</td>
+                                    <td class="px-[3rem]">${objListaPersonas[element].usuario}</td>
+                                    <td class="px-[3rem]">${objListaPersonas[element].email}</td>
+                                    <td class="px-[3rem]">${objListaPersonas[element].edad}</td>
+                                  </tr>`
+      });
+
+    }
+
+  }
+
+}
+
+
+function crearTabla() {
+  let objListaPersonas = JSON.parse(localStorage.getItem("listaPersonas"));
+  objListaPersonas.forEach(element => {
+    contenidoTabla.innerHTML +=
+      `<tr>
+      <td class="px-[3rem]">${element.area}</td>
+      <td class="px-[3rem]">${element.nombre} ${element.apellidos}</td>
+      <td class="px-[3rem]">${element.usuario}</td>
+      <td class="px-[3rem]">${element.email}</td>
+      <td class="px-[3rem]">${element.edad}</td>
+    </tr>`
+  });
+}
+
--- Mision_3/main2.js
+++ Mision_3/main2.js
@@ -42,23 +42,19 @@ const formulario = document.querySelector("#formulario");
 //bloquear formulario
 formulario.addEventListener('submit',(e)=>{
     e.preventDefault();
-})
-//guardar datos de 
-btnEnviar.addEventListener('click',()=>{
     //traer el obj JSOn que esta en el Local Storage y covertitir el objeto a lista y asignarlo a la lista de personas
    let listaPersonas = JSON.parse(localStorage.getItem('listaPersonas')) || [];   
 
-    //creo objeto personas
-    let persona = new Persona(nombres.value, apellidos.value, area.value, usuario.value, edad.value, direccion.value, email.value);
-    //pusheo elobj persona a la lista de personas
-    listaPersonas.push(persona);
-    //transformo en JSON la lista de personas
-    listaPersonas = JSON.stringify(listaPersonas);
-    //guardo en el loca storage el obj JSON de la lista de personas
-    localStorage.setItem("listaPersonas", listaPersonas);
-    //volver a la pag principal.
-    location.href = "index.html"; 
-    
+   //creo objeto personas
+   let persona = new Persona(nombres.value, apellidos.value, area.value, usuario.value, edad.value, direccion.value, email.value);
+   //pusheo elobj persona a la lista de personas
+   listaPersonas.push(persona);
+   //transformo en JSON la lista de personas
+   listaPersonas = JSON.stringify(listaPersonas);
+   //guardo en el loca storage el obj JSON de la lista de personas
+   localStorage.setItem("listaPersonas", listaPersonas);
+   //volver a la pag principal.
+   location.href = "index.html"; 
 })
 
 
