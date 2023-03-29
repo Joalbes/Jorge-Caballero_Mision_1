6c38203f1dca83ad14828214f8902c2167fdc863
Jorge A Caballero <joalbes@gmail.com>
2023-03-29 15:45:26 -0500

    version 3:45pm

--- Recuperacion/main.js
+++ Recuperacion/main.js
@@ -1,25 +1,35 @@
 //referencias a vistas
-const email = document.getElementsByClassName("email")
+const email = document.getElementById("email")
 const password = document.getElementById("password")
 const formulario = document.getElementById("formulario")
+const errorDatos = document.getElementById("errorDatos")
 
 //eventos
 formulario.addEventListener('submit',(e)=>{
-    e.preventDefault;
-    console.log(password.value);
-    console.log(password.email);
-
+    e.preventDefault();
+    main();
 })
 
 //funcion para caragar el local storage
 const main = async ()=> {
     const responses = await fetch('https://jsonplaceholder.typicode.com/users')
-    console.log(responses);
     let data = await responses.json();
-    data = JSON.stringify(data)
-    localStorage.setItem('data', data)
-
+    let data1 = JSON.stringify(data);
+    localStorage.setItem('data', data1);
+    let banderaCoincidencia = false;
+    data.forEach(element => {        
+        if ( email.value === element.email && password.value === element.username ) {
+            location.href = './index2.html'
+            banderaCoincidencia = true;
+        }   
+    });
+    if (!banderaCoincidencia) {
+        errorDatos.innerHTML = `
+        <p class="my-[0.3rem] text-sm p-[0.5rem] text-red-600"> Error: Datos no corresponden, intente nuevamente</p>`
+    }else{
+        errorDatos.innerHTML=``
+    }  
 }
 
 
-main()    
+   
